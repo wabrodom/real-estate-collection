a26dd668e4002d70c6832d8f4dfc1bac1194a98e
wabrodom <113647599+wabrodom@users.noreply.github.com>
2023-09-10 11:36:59 +0700

    change the view and theme to look better

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,24 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#5ed1ff",
+          secondary: "#9441d3",
+          accent: "#e09f67",
+          neutral: "#29232e",
+          "base-100": "#ffffff",
+          info: "#6eb9f2",
+          success: "#108467",
+          warning: "#fcd75f",
+          error: "#f31b3f",
+        },
+      },
+      "light",
+      "dark",
+      "corporate",
+    ],
+  },
 };
